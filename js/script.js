f3af660423d06cadcde84fe701dc0844c8ff7172
EC2 Default User <specialized.19991129@gmail.com>
2023-03-08 14:22:56 +0900

    [Update]最上部に戻るボタン

--- js/script.js
+++ js/script.js
@@ -6,3 +6,40 @@ $(function(){
     $('.spring').css({'opacity': '0.5'});
   });
 });
+
+$(function(){
+  $('.summer').mouseover(function(){
+    $('.summer').css({'opacity': '1'});
+  });
+  $('.summer').mouseout(function(){
+    $('.summer').css({'opacity': '0.5'});
+  });
+});
+
+$(function(){
+  $('.autumn').mouseover(function(){
+    $('.autumn').css({'opacity': '1'});
+  });
+  $('.autumn').mouseout(function(){
+    $('.autumn').css({'opacity': '0.5'});
+  });
+});
+
+$(function(){
+  $('.winter').mouseover(function(){
+    $('.winter').css({'opacity': '1'});
+  });
+  $('.winter').mouseout(function(){
+    $('.winter').css({'opacity': '0.5'});
+  });
+});
+
+// 最上部に移動するボタン
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
