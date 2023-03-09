522d7e539be49baaef4077309282e45cd44a51be
EC2 Default User <specialized.19991129@gmail.com>
2023-03-09 17:19:58 +0900

    [Update]マウスオーバー時のデザイン変更

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-$(function(){
-  $('.spring').mouseover(function(){
-    $('.spring').css({'opacity': '1'});
-  });
-  $('.spring').mouseout(function(){
-    $('.spring').css({'opacity': '0.5'});
-  });
-});
-
-$(function(){
-  $('.summer').mouseover(function(){
-    $('.summer').css({'opacity': '1'});
-  });
-  $('.summer').mouseout(function(){
-    $('.summer').css({'opacity': '0.5'});
-  });
-});
-
-$(function(){
-  $('.autumn').mouseover(function(){
-    $('.autumn').css({'opacity': '1'});
-  });
-  $('.autumn').mouseout(function(){
-    $('.autumn').css({'opacity': '0.5'});
-  });
-});
-
-$(function(){
-  $('.winter').mouseover(function(){
-    $('.winter').css({'opacity': '1'});
-  });
-  $('.winter').mouseout(function(){
-    $('.winter').css({'opacity': '0.5'});
-  });
-});
-
 // 最上部に移動するボタン
 $(function() {
   $('#back a').on('click',function(event){
